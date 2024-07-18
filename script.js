e61634845426e0e8e841ad74e4e3794b960b60e3
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-07-18 12:53:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-//complete this code
 class Animal {
-	constructor(species){
-		this.species=species
-	}
-	get species(){
-		 return (this.species)
-	}
-	makeSound(){
-		console.log("the"+ this.species + "makes a sound")
-	}
-}
-
-class Dog extends Animal {
-	  constructor(species) {
-    super(species);
+  constructor(species) {
+    this._species = species;
   }
-	purr(){
-		console.log("purr")
-	}
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("This animal makes a sound");
+  }
 }
 
 class Cat extends Animal {
-	  constructor(species) {
+  constructor(species) {
     super(species);
   }
-		bark(){
-		console.log("woof")
-	}
+
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  bark() {
+    console.log("woof");
+  }
 }
 
 // Do not change the code below this line
